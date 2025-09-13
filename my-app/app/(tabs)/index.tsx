3ca01566b723b699a40f8b28404d9a1010a2bca8
theoserrano <theorbserrano@gmail.com>
2025-09-13 16:55:57 -0300

    feat: criação da tela de login #1

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,31 +1,91 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { StyleSheet, TextInput, TouchableOpacity, SafeAreaView, KeyboardAvoidingView, Platform, Alert, View, Text } from 'react-native';
+import { useFonts } from "expo-font";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { styles } from './index.styles'; // Esta linha conecta com o arquivo que você criou
 
 export default function TabOneScreen() {
+  const [fontsLoaded] = useFonts({
+    Sansation: require("../../assets/fonts/Sansation-Regular.ttf"),
+    "Sansation-Bold": require("../../assets/fonts/Sansation-Bold.ttf")
+  });
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
+  if (!fontsLoaded) {
+    return null; // ou algum loading
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.keyboardView}>
+        <View style={styles.containerFirst}>
+
+          <Text style={styles.title}>ENTRE EM SUA CONTA</Text>
+          <View style={styles.separator} />
+          <View style={styles.containerMain}>
+            <Text style={styles.normalText}>Entre com seu login</Text>
+
+            <View style={styles.inputContainer}>
+              <Ionicons name="mail-outline" size={25} color="#ACA8A8" style={styles.icon} />
+              <TextInput
+                style={styles.inputs}
+                placeholder='E-mail'
+                placeholderTextColor="#ACA8A8"
+                keyboardType='email-address'
+                value={email}
+                onChangeText={setEmail} />
+            </View>
+
+            <View style={styles.inputContainer}>
+              <Ionicons name="lock-closed-outline" size={25} color="#ACA8A8" style={styles.icon} />
+              <TextInput
+                style={styles.inputs}
+                placeholder='Senha'
+                placeholderTextColor="#ACA8A8"
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}></TextInput>
+            </View>
+
+            <View style={styles.optionsContainer}>
+              <TouchableOpacity style={styles.rememberMeContainer} onPress={() => setRememberMe(!rememberMe)}>
+                <MaterialCommunityIcons
+                  name={rememberMe ? 'checkbox-marked-outline' : 'checkbox-blank-outline'}
+                  size={24}
+                  color="#8A8A8A"
+                />
+                <Text style={styles.optionsText}> Lembre-se de mim</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity>
+                <Text style={styles.optionsText}>Esqueci minha senha</Text>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity style={styles.loginButtonContainer} >
+              <Text style={styles.loginButtonText}>LOGIN</Text>
+            </TouchableOpacity>
+
+            <View style={styles.dividerContainer}>
+              <View style={styles.line} />
+              <Text style={styles.dividerText}>Ou</Text>
+              <View style={styles.line} />
+            </View>
+
+            <View style={styles.footerContainer}>
+              <Text style={styles.footerText}>Ainda não tem uma conta? </Text>
+              <TouchableOpacity>
+                <Text style={[styles.footerText, styles.registerLink]}>Registre-se</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
