2ca81047fd6035ed597d23387a2d56b9b3fdc785
Caio B. <123210218+0xC410@users.noreply.github.com>
2025-09-30 08:58:31 -0300

    fix: Adicionado a possibilidade de escolher entre temas

--- my-app/constants/Colors.ts
+++ my-app/constants/Colors.ts
@@ -1,7 +1,33 @@
-export const Colors = {
-  background: "#E4F3F8",
-  primaryColor: "#0A0F6D",
-  cardBackground: "#CCF0FF",
-  softWhite: "#FAFAFA",
-  inputTextColor: "#3A3636",
-};
+const colors = {
+primary: {
+  light: "#0A0F6D",
+  dark: "#0B79D0",
+},
+secondary: {
+  light: "#FF9800",
+  dark: "#F57C00",
+},
+background: {
+  light: "#E4F3F8",
+  dark: "#121212",
+},
+popupBackground: {
+  light: "#CCF0FF",
+  dark: "#1E1E1E",
+},
+boxBackground: {
+  light: "#FAFAFA",
+  dark: "#1E1E1E",
+},
+text: {
+  light: "#3A3636",
+  dark: "#FAFAFA",
+  muted: "#757575",
+},
+error: "#D32F2F",
+success: "#388E3C",
+warning: "#FBC02D",
+white: "#FAFAFA"
+} as const;
+
+export default colors;
