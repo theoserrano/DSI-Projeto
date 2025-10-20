008fafd14c92328ce2fda09ccf8077ba1731b17e
Caio B. <123210218+0xC410@users.noreply.github.com>
2025-10-20 16:15:42 -0300

    Delete: Removendo diretório de backend

--- backend/api/routes/track.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import searchTracks from "../controllers/tracks.controller.js";
-
-const router = Router();
-router.get("/search", searchTracks);
-
-export default router;
--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import doenv from 'dotenv'
-doenv.config()
-const supabaseUrl = 'https://irpdupwcwusqlwhljhrc.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-export default supabase;
-
